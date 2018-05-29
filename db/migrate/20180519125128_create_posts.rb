7664a8ced5233d409f034357ccce7e15326b7bec
Shaws <seanrichardparker@gmail.com>
2018-05-29 07:02:39 -0400

    remigrating posts

--- db/migrate/20180519125128_create_posts.rb
+++ db/migrate/20180519125128_create_posts.rb
@@ -1,11 +1,11 @@
-class CreatePosts < ActiveRecord::Migration
+class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.string :title
       t.string :author
       t.text :body
 
-      t.timestamps null: false
+      t.timestamps
     end
   end
 end
